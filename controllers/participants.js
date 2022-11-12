b7c27d557263e20d5a2d9d0844354bad4300cdc1
Lucas <lucasreis64@gmail.com>
2022-11-12 20:39:01 -0300

    style: formatting

--- controllers/participants.js
+++ controllers/participants.js
@@ -1,47 +1,54 @@
-import { users, messages } from "../data/mongoDB.js"
-import {validation} from "../validation/validation.js"
-import dayjs from 'dayjs'
+import { users, messages } from "../data/mongoDB.js";
+import { validation } from "../validation/validation.js";
+import dayjs from "dayjs";
 import { participantsSchema } from "../schemas/participants.js";
 
+export async function getParticipants(req, res) {
+    try {
+        const usersObj = await users.find().toArray();
+        const usersArr = usersObj.map((u) => u.name);
 
-
-export async function getParticipants (req, res) {
-    try{
-        const usersObj = await users.find().toArray()
-        const usersArr = usersObj.map((u)=>u.name)
-        
-        res.send(usersArr)
-    }catch (error) {
-        console.log(error)
+        res.send(usersArr);
+    } catch (error) {
+        console.log(error);
         res.status(500).send(error);
     }
 }
 
-export async function postParticipant (req, res) {
-    let now = dayjs()
-    const name = req.body.name
-    const isSameName = await users.findOne({name: name})
+export async function postParticipant(req, res) {
+    let now = dayjs();
+    const name = req.body.name;
+    const isSameName = await users.findOne({ name: name });
 
-    if (!validation(req.body, res, participantsSchema)) return
+    if (!validation(req.body, res, participantsSchema)) return;
 
-    if(existentUser(name, res, isSameName)) return
+    if (existentUser(name, res, isSameName)) return;
 
-    const messageObj = {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: now.format("HH:mm:ss")}
-    const userObj = {name: name, lastStatus:Date.now()}
+    const messageObj = {
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: now.format("HH:mm:ss"),
+    };
+    const userObj = { name: name, lastStatus: Date.now() };
 
-    try{
-        await users.insertOne(userObj)
-        await messages.insertOne(messageObj)
-        
-        res.status(201).send(userObj)
-    }catch (error) {
-        console.log(error)
+    try {
+        await users.insertOne(userObj);
+        await messages.insertOne(messageObj);
+
+        res.status(201).send(userObj);
+    } catch (error) {
+        console.log(error);
         res.status(500).send(error);
     }
 }
 
-function existentUser(user, response, same){
-    if (same) {response.status(422).send(`username "${user}" is already in use`); return true}
-    
-    return false
+function existentUser(user, response, same) {
+    if (same) {
+        response.status(422).send(`username "${user}" is already in use`);
+        return true;
+    }
+
+    return false;
 }
