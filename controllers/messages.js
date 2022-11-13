b95edb03d043c635df8018bcbfbb3f9b0866a793
Lucas <lucasreis64@gmail.com>
2022-11-13 01:01:51 -0300

    feat: messages get

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,16 +4,33 @@ import { messagesSchema } from "../schemas/messages.js";
 import dayjs from "dayjs";
 
 export async function getMessages(req, res) {
-    
+    const limit = Number(req.query.limit);
+    const user = req.headers.user;
+    let messageArr = await messages.find().toArray();
+
+    if (!user) {
+        res.sendStatus(422);
+        return;
+    }
+
+    if (!limit) {
+        res.send(messageArr);
+        return;
+    }
+
+    messageArr = filterMessages(messageArr, user, limit)
+
+    res.send(messageArr);
+    return;
 }
 
 export async function postMessage(req, res) {
     const from = req.headers.user;
     const messageObj = req.body;
-    const {to, text, type} = req.body
+    const { to, text, type } = req.body;
 
     if (!from) {
-        res.status(422).send('missing header field');
+        res.status(422).send("missing header field");
         return;
     }
 
@@ -27,9 +44,27 @@ export async function postMessage(req, res) {
     }
 
     let now = dayjs();
-    const message = {from: from, to: to, text: text, type: type, time: now.format("HH:mm:ss")}
+    const message = {
+        from: from,
+        to: to,
+        text: text,
+        type: type,
+        time: now.format("HH:mm:ss"),
+    };
 
-    await messages.insertOne(message)
+    await messages.insertOne(message);
 
-    res.statusSend(201);
+    res.sendStatus(201);
+}
+
+function filterMessages (msg, user, limit) {
+    msg = msg.filter(
+        ({ from, to, type }) =>
+            type === "message" ||
+            type === "status" ||
+            (type === "private_message" && (from === user || to === user || to === "Todos" || to === "todos"))
+    );
+
+    msg = msg.slice(-limit);
+    return msg
 }
