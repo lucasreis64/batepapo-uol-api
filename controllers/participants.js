820abc0bcba8cb4598aeb22da6fbe6f692591e66
Lucas <lucasreis64@gmail.com>
2022-11-13 12:53:59 -0300

    refactor: wrong position try

--- controllers/participants.js
+++ controllers/participants.js
@@ -18,25 +18,27 @@ export async function getParticipants(req, res) {
 export async function postParticipant(req, res) {
     let now = dayjs();
     const name = req.body.name;
-    const isSameName = await users.findOne({ name: name });
-
-    if (!validation(req.body, res, participantsSchema)) return;
-
-    if (isSameName) {
-        res.status(422).send(`username "${name}" is already in use`);
-        return
-    }
-
-    const messageObj = {
-        from: name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time: now.format("HH:mm:ss"),
-    };
-    const userObj = { name: name, lastStatus: Date.now() };
 
     try {
+        const isSameName = await users.findOne({ name: name });
+
+        if (!validation(req.body, res, participantsSchema)) return;
+
+        if (isSameName) {
+            res.status(422).send(`username "${name}" is already in use`);
+            return
+        }
+
+        const messageObj = {
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: now.format("HH:mm:ss"),
+        };
+        const userObj = { name: name, lastStatus: Date.now() };
+
+    
         await users.insertOne(userObj);
         await messages.insertOne(messageObj);
 
