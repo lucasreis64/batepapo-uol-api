ccff805ac2a34643299382594dc92b70eeb5c37d
Lucas <lucasreis64@gmail.com>
2022-11-13 14:52:20 -0300

    feat: remove inactive users

---
+++ Aux/removeInactive.js
@@ -0,0 +1,20 @@
+import dayjs from "dayjs";
+import { users, messages } from "../data/mongoDB.js";
+
+export async function removeInactive () {
+    try{
+        const maxTime = 10000
+        const usersArr = await users.find().toArray()
+        const deleteArr = usersArr.filter(({lastStatus})=>(Date.now() - lastStatus)>maxTime)
+        
+        deleteArr.forEach(async (d)=>{
+            let now = dayjs()
+            const id = d._id
+            await users.deleteOne({_id: id})
+            await messages.insertOne({from: d.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: now.format("HH:mm:ss")})
+        })
+    }
+    catch (error){
+        console.error(error)
+    }
+}
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import participantsRouter from '../routes/participants.js'
 import messagesRouter from '../routes/messages.js'
 import statusRouter from '../routes/status.js'
+import { removeInactive } from '../Aux/removeInactive.js'
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,8 @@ app.use(json())
 app.use('/participants', participantsRouter)
 app.use('/messages', messagesRouter)
 app.use('/status', statusRouter)
+
+setInterval(removeInactive, 15000)
 
 app.listen(5000, ()=>{
     console.log('Rodando!')
