22195b597dde21979c20b55825abab37f9052991
Lucas <lucasreis64@gmail.com>
2022-11-16 15:29:16 -0300

    chore: try and catch added

--- src/data/mongoDB.js
+++ src/data/mongoDB.js
@@ -4,7 +4,13 @@ import { MongoClient } from "mongodb";
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-await mongoClient.connect();
+
+try {
+    await mongoClient.connect();
+    console.log("MongoDB conectado!");
+} catch (err) {
+    console.log(err);
+}
 
 export const db = mongoClient.db("batepapo-uol-api");
 export const users = db.collection("participants");
